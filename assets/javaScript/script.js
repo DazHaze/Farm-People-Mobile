5ed57d276f89b09c4213f069c3db81aae4c9de6f
DazHaze <dhayes1.wex@gmail.com>
2021-10-24 15:02:56 +0000

    added click function and inner html to about buttons

--- assets/javaScript/script.js
+++ assets/javaScript/script.js
@@ -1,1 +1,16 @@
-console.log("Script is connected.");
+window.addEventListener('DOMContentLoaded', (event) => {
+    ClickAboutButtons();
+});
+
+function ClickAboutButtons(){
+    let buttons = document.getElementsByClassName('about-button');
+    for (let i = 0; i < buttons.length; i++) {
+        const button = buttons[i];
+        button.innerHTML = i;
+        button.setAttribute('data-type', i);
+        button.addEventListener('click', function ButtonClicked() {
+            console.log(button.getAttribute('data-type'));
+        })
+        
+    }
+}
